67a0f2d2c8d93020f4d7ab82d98eaabde20ab07c
Khimraj <khimrajsuthar@gmail.com>
2020-06-01 13:22:22 +0530

    Day 1

---
+++ Day_1_Invert_Binary_Tree.cpp
@@ -0,0 +1,49 @@
+/* Invert a binary tree.
+
+Example:
+
+Input:
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+Output:
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1 */
+
+
+/* Approach - dfs traversal */
+
+// Time Complexity - O(n), where n is number of nodes in tree
+// Space Complexity - O(1)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (root) {
+            TreeNode *tmp = root->left;
+            root->left = root->right;
+            root->right = tmp;
+            invertTree(root->left);
+            invertTree(root->right);
+        }
+        return root;
+    }
+};
